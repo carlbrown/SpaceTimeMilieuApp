adbbfb70c1440c00c70558ef1bcf6107710081c4
Carl Brown <carl.brown1@ibm.com>
2017-03-07 16:45:07 -0600

    Array-based plus map zoom

--- Sources/SpaceTimeMilieuApp/ViewController.swift
+++ Sources/SpaceTimeMilieuApp/ViewController.swift
@@ -13,7 +13,12 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView?
-    let initialLocation = Point(lat: 30.3672, latHemisphere: Point.LatitudeHemisphereEnum.north, long: -97.7431, longHemisphere: Point.LongitudeHemisphereEnum.west, datetime: Date(), timezone: "US/Central")
+    
+    private let initialLocation = Point(lat: 30.3672, latHemisphere: Point.LatitudeHemisphereEnum.north, long: -97.7431, longHemisphere: Point.LongitudeHemisphereEnum.west, datetime: Date(), timezone: "US/Central")
+    private let finalLocation = Point(lat: 30.5083, latHemisphere: Point.LatitudeHemisphereEnum.north, long: -97.6789, longHemisphere: Point.LongitudeHemisphereEnum.west, datetime: Date(timeIntervalSinceNow: 3600), timezone: "US/Central")
+    
+    private let regionMargin: Double = 0.01
+    private let initialViewPortMeters: CLLocationDistance = 5000
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +33,13 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(initialLocation.latitudeDegrees, initialLocation.longitudeDegrees),5000, 5000)
-//        self.mapView?.setRegion(coordinateRegion, animated: true)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate,initialViewPortMeters, initialViewPortMeters)
+        self.mapView?.setRegion(coordinateRegion, animated: true)
         var request = URLRequest(url: URL(string:"https://ssldemo.linuxswift.com:8090/api")!)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         do {
-            request.httpBody = try JSONSerialization.data(withJSONObject:initialLocation.toDictionary())
+            request.httpBody = try Point.encodeJSON(points: [initialLocation,finalLocation])
             let task = URLSession.shared.dataTask(with: request) { fetchData,fetchResponse,fetchError in
                 guard fetchError == nil else {
                     print("Got fetch Error: \(fetchError?.localizedDescription ?? "Error with no description")")
@@ -45,31 +50,31 @@ class ViewController: UIViewController, MKMapViewDelegate {
                     return
                 }
                 do {
-                    let json = try JSONSerialization.jsonObject(with: fetchData, options: .mutableContainers)
-      
-                    if (getenv("DEBUG")) != nil { //debug
-                        //OK to crash if debugging turned on
-                        let jsonForPrinting = try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-                        let jsonToPrint = String(data: jsonForPrinting, encoding: .utf8)
-                        print("result: \(jsonToPrint!)")
+                    let decorations: [Decoration] = try Decoration.decodeJSON(data: fetchData)
+                    
+                    //if (getenv("DEBUG")) != nil { //debug
+                    //OK to crash if debugging turned on
+                    let jsonToPrint = String(data: fetchData, encoding: .utf8)
+                    print("result: \(jsonToPrint!)")
+                    //}
+                    
+                    for decoration in decorations {
+                        if let point = decoration.point,
+                            let map = self.mapView {
+                            let newAnnotation = DecorationAnnotation(point: point, decoration: decoration)
+                            DispatchQueue.main.async {
+                                map.addAnnotation(newAnnotation)
+                            }
+                        }
                     }
-
-                    if let fetchedDict = json as? [String: Any],
-                    let pointDict = fetchedDict[Point.pointKey] as? [String: Any],
-                        let point = Point(fromDict:pointDict),
-                        let decorationDict = fetchedDict[Decoration.decorationKey] as? [String: Any],
-                        let decoration = Decoration(fromDict: decorationDict),
-                        let map = self.mapView {
-                        let newAnnotation = DecorationAnnotation(point: point, decoration: decoration)
-                        map.addAnnotation(newAnnotation)
-                        let coordinateRegion = MKCoordinateRegionMakeWithDistance(newAnnotation.coordinate,5000, 5000)
-                        self.mapView?.setRegion(coordinateRegion, animated: true)
-
+                    if let (center, latDelta, longDelta) = Point.region(Decoration.points(decorations)) {
+                        DispatchQueue.main.async {
+                            self.mapView?.setRegion(MKCoordinateRegionMake(center, MKCoordinateSpan(latitudeDelta: latDelta + self.regionMargin, longitudeDelta: longDelta + self.regionMargin)), animated: true)
+                        }
                     }
                 } catch {
                     print("Could not parse remote JSON Payload \(error)! Giving up!")
                 }
-
             }
             task.resume()
 
